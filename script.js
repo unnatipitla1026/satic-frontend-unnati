b7770e20e0cd5a2ce3269e5d472c2bc896447be7
Unnati Pitla <unnatipitla1026@gmail.com>
2026-02-20 21:55:50 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function openPopup(){
+    document.getElementById("lovePopup").classList.add("active");
+}
+
+function closePopup(){
+    document.getElementById("lovePopup").classList.remove("active");
+}
